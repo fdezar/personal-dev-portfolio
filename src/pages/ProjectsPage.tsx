cee558d728b17710e3bc83254dffa1b2f65666c6
Gabriel Fernandez <gfernandezareas@gmail.com>
2024-09-03 10:43:38 +0200

    keep advancing

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -1,7 +1,24 @@
 function ProjectsPage() {
 
     return (
-        <div>Hi</div>
+        <div>
+            <div>Hi</div>
+
+            <div className="result">
+                <div>
+                    <div id="result-image">
+
+                    </div>
+                    <div id="result-title-and-web">
+
+                    </div>
+                </div>
+            <a href="#" className="result-url">https://en.wikipedia.org/wiki/Gabriel_Fernandez</a>
+                <a href="#" className="result-title">Gabriel Fernandez - Wikipedia</a>
+                
+                <p className="result-snippet">Some info.</p>
+            </div>
+        </div>
     )
 }
 
--- src/pages/ResumePage.tsx
+++ src/pages/ResumePage.tsx
@@ -4,9 +4,20 @@ function ResumePage() {
         <>
             <div id="featured-snippet">
                 <h2>Gabriel Fernandez</h2>
-                <p>Gabriel Fernandez is a Full Stack Developer specialized in MERN Stack, with freeCodeCamp, EDTeam, Google Activate and Udemy Certificates, and Ironhack.</p>
-                <a href="https://en.wikipedia.org/wiki/Monkey_D._Luffy" id="featured-snippet-url">en.wikipedia.org</a>
+                <p>Full Stack Developer</p>
+                {/* <a href="https://en.wikipedia.org/wiki/Gabriel_Fernandez" id="featured-snippet-url">en.wikipedia.org</a> */}
             </div>
+
+            <div id="images-section">
+                <div id="images-container">
+                    <img src="/src/assets/images/results-page/gabri-ironhack-image.png" id="images-main-image" alt="G1" />
+                    <div id="images-subcontainer">
+                        <img src="/src/assets/images/results-page/silk-project.png" id="images-second-image" alt="G2" />
+                        <img src="/src/assets/images/results-page/exquisite-project.png" id="images-third-image" alt="G3" />
+                    </div>
+                </div>
+            </div>
+
             <div className="result">
                 <a href="#" className="result-title">Gabriel Fernandez - Wikipedia</a>
                 <a href="#" className="result-url">https://en.wikipedia.org/wiki/Gabriel_Fernandez</a>
@@ -16,16 +27,6 @@ function ResumePage() {
                 <a href="#" className="result-title">Gabriel - Programming Wiki - User</a>
                 <a href="#" className="result-url">https://programming.com/wiki/Gabriel</a>
                 <p className="result-snippet">Some info.</p>
-            </div>
-        
-            <div id="images-section">
-                <h2>Images</h2>
-                <div id="images-container">
-                    <img src="https://g.com/g1.jpg" alt="G1" />
-                    <img src="https://g.com/g2.jpg" alt="G2" />
-                    <img src="https://g.com/g3.jpg" alt="G3" />
-                    <img src="https://g.com/g4.jpg" alt="G4" />
-                </div>
             </div>
         
             <div id="news-section">
