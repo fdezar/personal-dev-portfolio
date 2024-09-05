2660f822d770072b190da78ab37eccfb9d821ab2
Gabriel Fernandez <gfernandezareas@gmail.com>
2024-09-05 18:08:05 +0200

    new data added

--- src/pages/EducationPage.tsx
+++ src/pages/EducationPage.tsx
@@ -1,3 +1,62 @@
+const educationArray = [
+    {
+        image: "/src//assets/images/education-page/ironhack-spain-logo.jpeg",
+        header: "Ironhack",
+        url: "https://silk-project.vercel.app",
+        title: "Web Development Bootcamp",
+        description: "Silk is a Single-Page Full Stack Productivity App that lets users create his own notes, Kanban with Drag and Drop (only frontend), Todolist and use a Pomodoro clock. Also features a CRUD user system with JSON Web Token authentication. Built with React.js, Vite, Material UI, Node.js, Express.js, MongoDB, and more."
+    },
+    {
+        image: "/src/assets/images/education-page/freecodecamp-logo.jpeg",
+        header: "Ironhack",
+        url: "https://silk-project.vercel.app",
+        title: "freeCodeCamp",
+        description: "Silk is a Single-Page Full Stack Productivity App that lets users create his own notes, Kanban with Drag and Drop (only frontend), Todolist and use a Pomodoro clock. Also features a CRUD user system with JSON Web Token authentication. Built with React.js, Vite, Material UI, Node.js, Express.js, MongoDB, and more."
+    },
+    {
+        image: "/src/assets/images/education-page/udemy-logo.jpeg",
+        header: "Udemy",
+        url: "https://silk-project.vercel.app",
+        title: "Udemy",
+        description: "Silk is a Single-Page Full Stack Productivity App that lets users create his own notes, Kanban with Drag and Drop (only frontend), Todolist and use a Pomodoro clock. Also features a CRUD user system with JSON Web Token authentication. Built with React.js, Vite, Material UI, Node.js, Express.js, MongoDB, and more."
+    },
+    {
+        image: "/src/assets/images/education-page/edteam-logo.jpeg",
+        header: "Ironhack",
+        url: "https://silk-project.vercel.app",
+        title: "EDTeam",
+        description: "Silk is a Single-Page Full Stack Productivity App that lets users create his own notes, Kanban with Drag and Drop (only frontend), Todolist and use a Pomodoro clock. Also features a CRUD user system with JSON Web Token authentication. Built with React.js, Vite, Material UI, Node.js, Express.js, MongoDB, and more."
+    },
+    {
+        image: "/src/assets/images/education-page/google-activate-logo.jpeg",
+        header: "Ironhack",
+        url: "https://silk-project.vercel.app",
+        title: "Google Activate",
+        description: "Silk is a Single-Page Full Stack Productivity App that lets users create his own notes, Kanban with Drag and Drop (only frontend), Todolist and use a Pomodoro clock. Also features a CRUD user system with JSON Web Token authentication. Built with React.js, Vite, Material UI, Node.js, Express.js, MongoDB, and more."
+    },
+    {
+        image: "/src/assets/images/education-page/university-of-barcelona-logo.jpeg",
+        header: "University of Barcelona",
+        url: "https://silk-project.vercel.app",
+        title: "Psychosocial Intervention Master's Degree",
+        description: "Silk is a Single-Page Full Stack Productivity App that lets users create his own notes, Kanban with Drag and Drop (only frontend), Todolist and use a Pomodoro clock. Also features a CRUD user system with JSON Web Token authentication. Built with React.js, Vite, Material UI, Node.js, Express.js, MongoDB, and more."
+    },
+    {
+        image: "/src/assets/images/education-page/university-of-girona-logo.jpeg",
+        header: "University of Girona",
+        url: "https://silk-project.vercel.app",
+        title: "Social Education Degree",
+        description: "Silk is a Single-Page Full Stack Productivity App that lets users create his own notes, Kanban with Drag and Drop (only frontend), Todolist and use a Pomodoro clock. Also features a CRUD user system with JSON Web Token authentication. Built with React.js, Vite, Material UI, Node.js, Express.js, MongoDB, and more."
+    },
+    {
+        image: "/src/assets/images/projects-page/silk-logo.png",
+        header: "University of Girona",
+        url: "https://silk-project.vercel.app",
+        title: "B2 English Certificate",
+        description: "Silk is a Single-Page Full Stack Productivity App that lets users create his own notes, Kanban with Drag and Drop (only frontend), Todolist and use a Pomodoro clock. Also features a CRUD user system with JSON Web Token authentication. Built with React.js, Vite, Material UI, Node.js, Express.js, MongoDB, and more."
+    },
+]
+
 function EducationPage() {
 
     return (
--- src/pages/ExperiencePage.tsx
+++ src/pages/ExperiencePage.tsx
@@ -1,3 +1,34 @@
+const experienceArray = [
+    {
+        image: "/src//assets/images/education-page/ironhack-spain-logo.jpeg",
+        header: "Coordinadora d'Entitats de Poble Sec",
+        url: "jan. 2023 - apr. 2023",
+        title: "Psychosocial Interventor",
+        description: ""
+    },
+    {
+        image: "/src//assets/images/education-page/ironhack-spain-logo.jpeg",
+        header: "Coordinadora d'Entitats de Poble Sec",
+        url: "jan. 2023 - apr. 2023",
+        title: "Psychosocial Interventor",
+        description: ""
+    },
+    {
+        image: "/src//assets/images/education-page/ironhack-spain-logo.jpeg",
+        header: "Coordinadora d'Entitats de Poble Sec",
+        url: "jan. 2023 - apr. 2023",
+        title: "Psychosocial Interventor",
+        description: ""
+    },
+    {
+        image: "/src//assets/images/education-page/ironhack-spain-logo.jpeg",
+        header: "Coordinadora d'Entitats de Poble Sec",
+        url: "jan. 2023 - apr. 2023",
+        title: "Psychosocial Interventor",
+        description: ""
+    },
+]
+
 function ExperiencePage() {
 
     return (
