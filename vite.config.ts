24a50815e9d9161dadac3e87c8247edac348b52e
Gabriel Fernandez <gfernandezareas@gmail.com>
2024-11-19 19:29:48 +0100

    spa bug fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      }
-    }
-  },
   plugins: [react()],
 })
