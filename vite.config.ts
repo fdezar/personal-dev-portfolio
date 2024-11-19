f77ec181521b60800a314377e0c61e117793974a
Gabriel Fernandez <gfernandezareas@gmail.com>
2024-11-19 19:27:34 +0100

    trying to bug fix spa

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
@@ -6,7 +6,6 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: process.env.VITE_PROXY_URL,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       }
